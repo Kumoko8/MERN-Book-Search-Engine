77f8f2ff74c63bc4a4f2c5ace0fc1e0a1c38fb5f
Kumori <tauen8@gmail.com>
2023-10-12 12:02:01 -0500

    fixed bookschema and removed unnecessary seed files

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,6 +1,54 @@
+// const { Schema, model } = require('mongoose');
+
+// // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+// const bookSchema = new Schema({
+//   authors: [
+//     {
+//       type: String,
+//     },
+//   ],
+//   description: {
+//     type: String,
+//     required: true,
+//   },
+//   // saved book id from GoogleBooks
+//   bookId: {
+//     type: String,
+//     required: true,
+//   },
+//   image: {
+//     type: String,
+//   },
+//   link: {
+//     type: String,
+//   },
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+// });
+// // set up pre-save middleware to create password
+// bookSchema.pre('save', async function (next) {
+//   if (this.isNew || this.isModified('password')) {
+//     const saltRounds = 10;
+//     this.password = await bcrypt.hash(this.password, saltRounds);
+//   }
+
+//   next();
+// });
+
+// // compare the incoming password with the hashed password
+// bookSchema.methods.isCorrectPassword = async function (password) {
+//   return bcrypt.compare(password, this.password);
+// };
+
+// const Book = model('Book', bookSchema);
+
+// module.exports = Book;
+
+
 const { Schema } = require('mongoose');
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const bookSchema = new Schema({
   authors: [
     {
@@ -11,7 +59,6 @@ const bookSchema = new Schema({
     type: String,
     required: true,
   },
-  // saved book id from GoogleBooks
   bookId: {
     type: String,
     required: true,
@@ -27,21 +74,8 @@ const bookSchema = new Schema({
     required: true,
   },
 });
-// set up pre-save middleware to create password
-bookSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
 
-  next();
-});
 
-// compare the incoming password with the hashed password
-bookSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
-};
+module.exports = bookSchema;
 
-const Book = model('Book', bookSchema);
 
-module.exports = Book;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +14,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+//you were working in back-end on the seeders files (9-26)
